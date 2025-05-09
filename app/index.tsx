30cc7242757ccd08b7f4978aebe4010d2d895dc5
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-05-10 00:25:13 +0800

    feat: connects to supabase

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,22 @@
+import { supabase } from '@/utils/supabase';
+import React from 'react';
 import { View } from 'react-native';
 
 export default function Root() {
+  React.useEffect(() => {
+    supabase
+      .from('todos')
+      .select('*')
+      .then(({ data, error }) => {
+        if (error) {
+          console.error(error.message);
+          throw new Error(error.message);
+        }
+
+        if (data) {
+          console.log(data);
+        }
+      });
+  });
   return <View />;
 }
